b6b0259c6262602a7241a58738a2c3a62f04f505
artemcoolac <us210716@yandex.ru>
2019-10-14 02:06:09 +0300

    nothing important

--- web-components/src/index.js
+++ web-components/src/index.js
@@ -3,6 +3,6 @@ import './index.css';
 import './components/FormInput';
 import './components/MessageForm';
 import  './components/ChatHeader';
-import './components/ChatField';
+//import './components/ChatField';
 import './components/MessageShell';
 import './components/HistoryDate'
