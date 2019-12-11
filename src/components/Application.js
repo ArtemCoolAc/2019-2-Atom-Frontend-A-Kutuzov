02a2a0b61da5eefb3fd11a1bfed1f53fa0e4e111
artemcoolac <us210716@yandex.ru>
2019-12-12 00:16:33 +0300

    some rfixes

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,58 +1,58 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { ControlChatList } from './ControlChatList'
-import { ControlChat } from './ControlChat'
-import { UserProfile } from './Profile'
-import { FakeData } from './FakeData'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ControlChatList } from './ControlChatList';
+import { ControlChat } from './ControlChat';
+import { UserProfile } from './Profile';
+import { FakeData } from './FakeData';
 
 export class Application extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      activeChat: null,
-    }
+		this.state = {
+			activeChat: null,
+		};
 
-    this.setCurrentChat = this.setCurrentChat.bind(this)
+		this.setCurrentChat = this.setCurrentChat.bind(this);
 
-    FakeData()
-  }
+		FakeData();
+	}
 
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('activeChat')
-      const chatId = JSON.parse(json)
+	componentDidMount() {
+		try {
+			const json = localStorage.getItem('activeChat');
+			const chatId = JSON.parse(json);
 
-      if (chatId >= 0) {
-        this.setState({
-          activeChat: chatId,
-        })
-      }
-    } catch (Error) {
-      localStorage.clear()
-    }
-  }
+			if (chatId >= 0) {
+				this.setState({
+					activeChat: chatId,
+				});
+			}
+		} catch (Error) {
+			localStorage.clear();
+		}
+	}
 
-  setCurrentChat(id) {
-    this.setState({ activeChat: id })
-    localStorage.setItem('activeChat', JSON.stringify(id))
-  }
+	setCurrentChat(id) {
+		this.setState({ activeChat: id });
+		localStorage.setItem('activeChat', JSON.stringify(id));
+	}
 
-  render() {
-    const { activeChat } = this.state
+	render() {
+		const { activeChat } = this.state;
 
-    return (
-      <Router>
-        <Switch>
-          <Route path="/2019-2-Atom-Frontend-A-Kutuzov" exact>
-            <ControlChatList setActiveChat={this.setCurrentChat} />
-          </Route>
-          <Route path="/chat">
-            <ControlChat activeChat={activeChat} />
-          </Route>
-          <Route path="/profile" component={UserProfile} />
-        </Switch>
-      </Router>
-    )
-  }
+		return (
+			<Router basename={'/2019-2-Atom-Frontend-A-Kutuzov'}>
+				<Switch>
+					<Route path="/" exact>
+						<ControlChatList setActiveChat={this.setCurrentChat} />
+					</Route>
+					<Route path="/chat">
+						<ControlChat activeChat={activeChat} />
+					</Route>
+					<Route path="/profile" component={UserProfile} />
+				</Switch>
+			</Router>
+		);
+	}
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,36 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from '../static/styles/Profile.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../static/styles/Profile.module.css';
 
 export function UserProfile(props) {
-  return (
-    <div className={styles.sheet}>
-      <div className={styles.toolBar}>
-        <Link to="/2019-2-Atom-Frontend-A-Kutuzov">
-          <div className={styles.backButton} />
-        </Link>
-        <div className={styles.title}>Редактируем профиль</div>
-        <Link to={'/2019-2-Atom-Frontend-A-Kutuzov'}>
-          <div className={styles.confirm} />
-        </Link>
-      </div>
-      <div className={styles.avatar} />
-      <div className={styles.Box}>
-        <div className={styles.information}>Ваша фамилия и имя</div>
-        <input className={styles.customInput} placeholder="Да вы известный человек:)" type="text" />
-      </div>
-      <div className={styles.Box} style={{ marginBottom: '80px' }}>
-        <div className={styles.information}>Username</div>
-        <input className={styles.customInput} placeholder="Введите никнейм" type="text" />
-        <div className={styles.attachment}>Минимально допустимая длина - 5 символов</div>
-      </div>
-      <div className={styles.Box} style={{ height: '120px' }}>
-        <div className={styles.information}>О себе</div>
-        <input className={styles.customInput} placeholder="Порадуйте нас!" type="text" />
-        <div className={styles.attachment} style={{ paddingTop: '16px' }}>
-          Напишите о себе, мы обязательно проанализируем и вычислим вас по IP
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.sheet}>
+			<div className={styles.toolBar}>
+				<Link to="/">
+					<div className={styles.backButton} />
+				</Link>
+				<div className={styles.title}>Редактируем профиль</div>
+				<Link to={'/'}>
+					<div className={styles.confirm} />
+				</Link>
+			</div>
+			<div className={styles.avatar} />
+			<div className={styles.Box}>
+				<div className={styles.information}>Ваша фамилия и имя</div>
+				<input
+					className={styles.customInput}
+					placeholder="Да вы известный человек:)"
+					type="text"
+				/>
+			</div>
+			<div className={styles.Box} style={{ marginBottom: '80px' }}>
+				<div className={styles.information}>Username</div>
+				<input
+					className={styles.customInput}
+					placeholder="Введите никнейм"
+					type="text"
+				/>
+				<div className={styles.attachment}>
+					Минимально допустимая длина - 5 символов
+				</div>
+			</div>
+			<div className={styles.Box} style={{ height: '120px' }}>
+				<div className={styles.information}>О себе</div>
+				<input
+					className={styles.customInput}
+					placeholder="Порадуйте нас!"
+					type="text"
+				/>
+				<div className={styles.attachment} style={{ paddingTop: '16px' }}>
+					Напишите о себе, мы обязательно проанализируем и вычислим вас по IP
+				</div>
+			</div>
+		</div>
+	);
 }
