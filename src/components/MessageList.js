d65d275a108305bac6233ed44a1c07679f1ad869
artemcoolac <us210716@yandex.ru>
2019-12-11 23:45:08 +0300

    some fixes228

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -10,20 +10,12 @@ export function MessageList(props) {
 	if (activeChat >= 0 && messageMap[activeChat]) {
 		// eslint-disable-next-line
 		messageMap[activeChat].map((item) => {
-			const ref = React.createRef();
 			if (item.owner === 'self') {
 				const Message = (
 					<div className={styles.messageBox} key={iter++}>
 						<MessageBox shift="self" content={item} isAudio={isAudio} />
 					</div>
 				);
-				//const {id} = Message.props;
-				const elem = document.getElementById('.messageWrap');
-				// console.log(elem);
-				// const elems = document.getElementsByClassName('MessageBox_messageBox');
-				// elems[-1].scrollIntoView(true);
-				// console.log(elems);
-				// Message.scrollIntoView(true);
 				list.push(Message);
 			} else if (item.owner === 'outside') {
 				const Message = (
