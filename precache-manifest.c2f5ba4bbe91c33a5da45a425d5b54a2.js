7b969464b70554fad6780275915bc605474fcce6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 15:40:41 +0000

    Deploy ArtemCoolAc/2019-2-Atom-Frontend-A-Kutuzov to github.com/ArtemCoolAc/2019-2-Atom-Frontend-A-Kutuzov.git:gh-pages

---
+++ precache-manifest.c2f5ba4bbe91c33a5da45a425d5b54a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6622b416936ea35e6f1ecb9c56b00ef9",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/index.html"
+  },
+  {
+    "revision": "2893c43a66f00a549b79",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/css/main.3c1e19fa.chunk.css"
+  },
+  {
+    "revision": "c17da4d3b99b684e3135",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/js/2.20289cfa.chunk.js"
+  },
+  {
+    "revision": "4fdc00fbc861a978a4476cc8b3c4086f",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/js/2.20289cfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2893c43a66f00a549b79",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/js/main.608b15b6.chunk.js"
+  },
+  {
+    "revision": "bbc7b6c8665e73f7ca85",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/js/runtime-main.b3162d30.js"
+  },
+  {
+    "revision": "f17c0259cee5837c790f6b5eee91524b",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/Ava1.f17c0259.jpg"
+  },
+  {
+    "revision": "efc8c52785b928b741b037bab6cbbac8",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/cancel.efc8c527.png"
+  },
+  {
+    "revision": "540085f5315eb553de8415cbbc2d1101",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/confirm.540085f5.png"
+  },
+  {
+    "revision": "4abb2422222939a4546eb3f82d1f71d5",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/new1.4abb2422.png"
+  },
+  {
+    "revision": "44677cb1fe3201b91128b29fc97987ad",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/opt.44677cb1.png"
+  },
+  {
+    "revision": "1c5706491d087851fa4ba52714f29fe5",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/return_but.1c570649.png"
+  },
+  {
+    "revision": "71c66c17497544cc23881d781a942cac",
+    "url": "/2019-2-Atom-Frontend-A-Kutuzov/static/media/searching.71c66c17.webp"
+  }
+]);
